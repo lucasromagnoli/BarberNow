03f852b43d996d97f544ab2912b00fe165f3192f
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-05-06 19:37:17 -0300

    Feature - Autenticando o usuário, buscando no banco de dados e confirmando se o mesmo está cadastrado.

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
+routes.get('/', SessionController.create)
 routes.get('/signup', UserController.create)
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
+routes.post('/signin', SessionController.store)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
